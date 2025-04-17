d7e3ba96e68b9617a9fd5df047ad52dcb527e082
Marianne Dehasque <marideha@login1.hsn.dardel.pdc.kth.se>
2025-04-17 15:50:08 +0200

    update for Elemax reference

--- genotyping_filtering/bcftools_filter_missing.sh
+++ genotyping_filtering/bcftools_filter_missing.sh
@@ -1,17 +1,24 @@
-#!/bin/bash -l
-#SBATCH -A naiss2024-5-54
-#SBATCH -p core
-#SBATCH -n 8
-#SBATCH -t 1-00:00:00
-#SBATCH -J bcftools_missing
+#!/bin/bash -le
+
+#SBATCH -A naiss2025-5-78
+#SBATCH --job-name=filter_missing
+#SBATCH --partition=shared
+#SBATCH --cpus-per-task=24
+#SBATCH --time=1-00:00:00
+#SBATCH --error=slurm-%x-%j-%N-%A.out
+#SBATCH --output=slurm-%x-%j-%N-%A.out
 
 #Load modules
-module load bioinfo-tools bcftools/1.14
+module load bioinfo-tools bcftools/1.20
+module load tabix/0.2.6
 
-DIR="/proj/sllstore2017093/mammoth/marianne/GIT/BCM_data" #output directory
-files_to_merge="/proj/sllstore2017093/mammoth/marianne/GIT/BCM/genotyping_filtering/bcftools_merge_files.txt" #File containing absolute path of all files to merge. One file per line.
-MERGED_BCF="BCM_high_coverage" #Name of the final merged BCF file
+DIR="/cfs/klemming/projects/supr/sllstore2017093/mammoth/marianne/GIT/data_BCM/BCF" #output directory
+files_to_merge="/cfs/klemming/projects/supr/sllstore2017093/mammoth/marianne/GIT/BCM/genotyping_filtering/INPUTFILES/bcftools_merge_files.txt" #File containing absolute path of all files to merge. One file per line.
+MERGED_BCF="BCM_high_coverage_EleMax" #Name of the final merged BCF file
 
 #Remove sites with missing sites
 bcftools view -i 'F_MISSING=0' $DIR/${MERGED_BCF}.bcf -Ob -o $DIR/${MERGED_BCF}.Fmiss0.bcf
 bcftools index $DIR/${MERGED_BCF}.Fmiss0.bcf
+
+bcftools convert -O z -o $DIR/${MERGED_BCF}.Fmiss0.vcf.gz $DIR/${MERGED_BCF}.Fmiss0.bcf
+tabix $DIR/${MERGED_BCF}.Fmiss0.vcf.gz
--- genotyping_filtering/bcftools_merge.sh
+++ genotyping_filtering/bcftools_merge.sh
@@ -1,19 +1,20 @@
-#!/bin/bash -l
-#SBATCH -A naiss2024-5-54
-#SBATCH -p core
-#SBATCH -n 8
-#SBATCH -t 2-00:00:00
-#SBATCH -J bcftools_merge
+#!/bin/bash -le
+
+#SBATCH -A naiss2025-5-78
+#SBATCH --job-name=merge
+#SBATCH --partition=shared
+#SBATCH --cpus-per-task=24
+#SBATCH --time=1-00:00:00
+#SBATCH --error=slurm-%x-%j-%N-%A.out
+#SBATCH --output=slurm-%x-%j-%N-%A.out
 
 #Load modules
-module load bioinfo-tools bcftools/1.14 samtools/1.14
-module load BEDTools/2.29.2
-module load tabix/0.2.6
+module load bioinfo-tools bcftools/1.20
 
-DIR="/proj/sllstore2017093/mammoth/marianne/GIT/BCM_data" #output directory
-files_to_merge="/proj/sllstore2017093/mammoth/marianne/GIT/BCM/genotyping_filtering/bcftools_merge_files.txt" #File containing absolute path of all files to merge. One file per line.
-MERGED_BCF="BCM_high_coverage" #Name of the final merged BCF file
+DIR="/cfs/klemming/projects/supr/sllstore2017093/mammoth/marianne/GIT/data_BCM/BCF" #output directory
+files_to_merge="/cfs/klemming/projects/supr/sllstore2017093/mammoth/marianne/GIT/BCM/genotyping_filtering/INPUTFILES/bcftools_merge_files.txt" #File containing absolute path of all files to merge. One file per line.
+MERGED_BCF="BCM_high_coverage_EleMax" #Name of the final merged BCF file
 
 #Merge bcf files into one bcf file, allowing multiallelic SNP records
-cat $files_to_merge | xargs bcftools merge -m snps --threads 8 -Ob -o $DIR/${MERGED_BCF}.bcf
+cat $files_to_merge | xargs bcftools merge -m snps --threads 24 -Ob -o $DIR/${MERGED_BCF}.bcf
 bcftools index $DIR/${MERGED_BCF}.bcf
